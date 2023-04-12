847f6f24b79148b9eb9205ae3ca7b2d7b50c4481
P4CKt <omurcanuslu@outlook.com>
2023-04-12 22:44:07 +0300

    heroku run

--- api/server.js
+++ api/server.js
@@ -8,9 +8,7 @@ server.use(express.json());
 
 server.use("/api/users", userRouter);
 server.use("/api/post", verifyUser, postRouter);
-server.use("*", (req, res) => {
-  res.status(404).json({ message: "not found" });
-});
+
 server.use((err, req, res) => {
   res.status(err.status || 500).json({
     message: err.message,
