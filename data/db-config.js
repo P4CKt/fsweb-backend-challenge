ba113fb07ba8d69654ec62fd11fb00b3c8661f60
P4CKt <omurcanuslu@outlook.com>
2023-04-12 21:48:36 +0300

    herokuu

--- data/db-config.js
+++ data/db-config.js
@@ -1,6 +1,6 @@
 const knexConfig = require("../knexfile");
 
 const knex = require("knex");
-const { NODE_ENV } = require("../config/config");
+const { NODE_ENV } = require("../config/index");
 
 module.exports = knex(knexConfig[NODE_ENV]);
